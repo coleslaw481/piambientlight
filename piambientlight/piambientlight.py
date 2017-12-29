35a7367a96786d615f5ff67abfc47f7ef0109e1b
Chris Churas <churas@gmail.com>
2017-12-28 22:56:20 -0800

    lots of changes. Still prototyping, but added an overlay with rectangles filled with the color found in the pixel locations on the camera. Also slowed framerate down to 1 per second.

--- piambientlight/piambientlight.py
+++ piambientlight/piambientlight.py
@@ -5,28 +5,39 @@ import time
 import threading
 import picamera
 import os
-
+import math
 from PIL import Image
+from PIL import ImageDraw
 
 # Create a pool of image processors
 done = False
 pval = 'na'
 lock = threading.Lock()
 pool = []
+pixel_values_changed = False
+camera_res = (640, 384)
 
-camera_res = (640, 360)
+overlay_img = Image.new('RGB', camera_res)
+BOX_COLOR = (0,255,0)
+BOX_WIDTH = 5
+
+draw = ImageDraw.Draw(overlay_img)
 
 pixels = []
 # generate top and bottom pixel array
-for x in xrange(10, camera_res[0] - 10, int(camera_res[0] / 6)):
+x_step = int(camera_res[0] / 6)
+for x in xrange(10, camera_res[0] + x_step, x_step):
+    if x > camera_res[0]:
+        x = camera_res[0]-10
     pixels.append((x, 10))
     pixels.append((x, camera_res[1] - 10))
-
+    
 # generate left and right pixels
 for y in xrange(60, 350, 50):
     pixels.append((10, y))
     pixels.append((630, y))
 
+pixels_values = [(0,0,0)]*len(pixels)
 
 class ImageProcessor(threading.Thread):
     def __init__(self):
@@ -40,6 +51,7 @@ class ImageProcessor(threading.Thread):
         # This method runs in a separate thread
         global done
         global pval
+        global pixel_values_changed
         while not self.terminated:
             # Wait for an image to be written to the stream
             if self.event.wait(1):
@@ -49,14 +61,25 @@ class ImageProcessor(threading.Thread):
                     # Read the image and do some processing on it
                     img = Image.open(self.stream)
                     pval = ''
-                    print '\n\n\n\n\n\n\n\n\n\n\n'
+                    i = 0
+                    # this is a flawed approach to only change overlay
+                    # if pixel intensities increase by more then 10 per color
+                    # cause we updated the pixels_values each time so
+                    # if color change is gradual enough the overlay
+                    # will never be updated. Should change code to update
+                    # previous values only if threshold is reached, course 
+                    # that means a double iteration across the pixels...
+                    pixel_values_changed = False
                     for p in pixels:
-                        pval = str(p) + '=>' + str(img.getpixel(p))
-                        print pval + ' '
-                    if len(pval) > 255:
-                        pval = pval[0:254]
-
-                    # print('160x120 = ' + str(img.getpixel((160,120))) + '\n')
+                       mypix = img.getpixel(p)
+                       for gg in xrange(0,3):  
+                         if int(math.fabs(mypix[gg]-pixels_values[i][gg])) > 10:
+                             pixel_values_changed = True
+                             break
+                       
+                       pixels_values[i] = mypix
+                       draw.rectangle([p,(p[0]+BOX_WIDTH,p[1]+BOX_WIDTH)],fill=mypix)
+                       i += 1
                     if os.path.isfile('exit'):
                         print('exit file detected\n')
                         done = True
@@ -77,10 +100,18 @@ class ImageProcessor(threading.Thread):
 
 
 def streams(camera):
+    over_lay = None
     while not done:
         with lock:
             if pool:
                 camera.annotate_text = pval
+                
+                if pixel_values_changed:
+                    if over_lay is not None:
+                        camera.remove_overlay(over_lay)
+                    over_lay = camera.add_overlay(overlay_img.tostring(), size=camera_res)
+                    over_lay.alpha = 128
+                    over_lay.layer = 3
                 processor = pool.pop()
             else:
                 processor = None
@@ -96,8 +127,8 @@ def streams(camera):
 with picamera.PiCamera() as camera:
     try:
         pool = [ImageProcessor() for i in range(4)]
-        camera.resolution = (640, 360)
-        camera.framerate = 30
+        camera.resolution = camera_res
+        camera.framerate = 1
         camera.start_preview()
         time.sleep(2)
         camera.capture_sequence(streams(camera), use_video_port=True)
